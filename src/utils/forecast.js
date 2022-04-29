f6a180f5c71615a9df56e4658db5d98103b931cc
Claude Duplissey <cvd0301@aol.com>
2022-04-29 12:36:08 -0400

    Add humidity status

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,8 @@ const forecast = (longitude, latitude, callback) => {
 //                        + " It feels like " + response.body.current.feelslike + " degrees out")
             callback(undefined, body.current.weather_descriptions[0] + ". It is currently "
                           + body.current.temperature + " degrees out."
-                          + " It feels like " + body.current.feelslike + " degrees out")
+                          + " The humidity is " + body.current.humidity + "."
+                          + " It feels like " + body.current.feelslike + " degrees out.")
         }
     })
 }
